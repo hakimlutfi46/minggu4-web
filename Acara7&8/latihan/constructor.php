0f095700e401110e34e19802ab42588672a988bb
hakimlutfi46 <lutfihakim1606@gmail.com>
2023-09-15 10:02:19 +0700

    add acara 10

--- Acara7&8/latihan/constructor.php
+++ Acara7&8/latihan/constructor.php
@@ -6,8 +6,8 @@ class mahasiswa
 
     function __construct($name, $nim)
     {
-        $this->name = "Nama : " . $name . "<br>";
-        $this->nim = "NIM : " . $nim . "<br>";
+        echo $this->name = "Nama : " . $name . "<br>";
+        echo $this->nim = "NIM : " . $nim . "<br>";
     }
     function get_name()
     {
@@ -22,10 +22,5 @@ class mahasiswa
 
 
 $mahasiswa1 = new mahasiswa("Muhammad Lutfi Hakim", "E41221867");
-$mahasiswa2 = new mahasiswa("Farhan Arianto", "E41225892");
-
-echo $mahasiswa1->get_name();
-echo $mahasiswa1->get_nim();
 echo "<br>";
-echo $mahasiswa2->get_name();
-echo $mahasiswa2->get_nim();
+$mahasiswa2 = new mahasiswa("Farhan Arianto", "E41225892");
